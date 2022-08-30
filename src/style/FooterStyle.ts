5f33611be7585191db1d36127134b885b2e9462e
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-31 00:04:33 +0900

    Style: FooterStyle.ts Create && application

---
+++ src/style/FooterStyle.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const FooterStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #000;
+  .footer {
+    padding: 2rem;
+  }
+  img {
+    display: inline-block;
+    border: 5px solid #fff;
+    border-radius: 50%;
+    background-color: #fff;
+  }
+  span {
+    display: block;
+    color: #fff;
+    padding-top: 1rem;
+  }
+`;
